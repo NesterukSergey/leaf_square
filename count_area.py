b73731ffc29d863367e12c13c210cb43a8207a0e
NesteruS <nesterus@protonmail.com>
2019-03-06 03:20:55 +0300

    Saving areas to file

---
+++ count_area.py
@@ -0,0 +1,174 @@
+import os
+from skimage import io
+from skimage.color import rgb2hsv
+from skimage.filters import threshold_otsu
+from skimage.filters.rank import median
+from skimage.morphology import disk
+import matplotlib.pyplot as plt
+import cv2
+import csv
+import pandas as pd
+
+
+def is_blurred(img):
+    '''
+    Detects if the image is blurred
+
+    :param img: np.ndarray
+    :return: boolean, True if blurred
+    '''
+    variance = cv2.Laplacian(img, cv2.CV_64F).var()
+    return variance < 40
+
+
+def split_into_9_parts(image):
+    '''
+    Splits image into 9 parts
+    |0|1|2|
+    |3|4|5|
+    |6|7|8|
+
+    :param image: numpy.ndarray
+    :return: list of 9 numpy.ndarrays
+    '''
+    vert = image.shape[0] // 3
+    hor = image.shape[1] // 3
+    ls = []
+
+    for col in range(3):
+        for row in range(3):
+            ls.append(image[vert * col: vert * (col + 1),
+                      hor * row: hor * (row + 1)])
+
+    return ls
+
+
+def count_green_pixels(img):
+    '''
+    Counts pixels in the image with high saturation using Otsu threshold
+    Asserts that image background if white!
+
+    :param img: np.ndarray
+    :return: int
+    '''
+    hsv_image = rgb2hsv(img)
+    saturation = hsv_image[:, :, 1]
+    threshold = threshold_otsu(saturation)
+    saturation = saturation > threshold * 1.3
+    saturation = median(saturation, disk(2))  # filtering
+
+    # show_image(saturation)
+    pixels_count = saturation.mean() * saturation.shape[0] * saturation.shape[1] / saturation.max()
+    return pixels_count
+
+
+def count_pixel_square(img):
+    '''
+    :param img: ndarray, - image with 2 reference squares
+    :return: one pixel square (cm^2)
+    '''
+    one_square = count_green_pixels(img) // 2
+    return 1 / one_square
+
+
+def write_areas_to_file(array, plant_number):
+    '''
+    Writes plant area series to [plant_number].csv file
+
+    :param array: array of areas
+    :param plant_number:
+    :return: filename
+    '''
+    file = str(plant_number) + '_plant_areas.csv'
+    with open(file, 'w', newline='') as csvfile:
+        time = ['time']
+        area = ['area']
+        for measurement in range(len(array)):
+            time.append(array[measurement]['time'])
+            area.append(array[measurement]['area'])
+        rows = zip(time, area)
+
+        writer = csv.writer(csvfile)
+        for row in rows:
+            writer.writerow(row)
+
+    return file
+
+
+def read_areas_from_file(plant_number, type='pandas_series'):
+    file = str(plant_number) + '_plant_areas.csv'
+
+    if type == 'array':
+        areas_array = []
+        with open(file, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                areas_array.append(row[1])
+
+        areas_array.pop(0)
+        return [float(s) for s in areas_array]
+
+    return pd.read_csv(file, header=0)
+
+
+def calculate_squares(images, write_to_file=True):
+    '''
+    Adds calculated squares of plants' leafs from the desk to the storage
+    |0|1|2|
+    |3| | |
+    |4|5|6| i.e. excluding reference square and empty part
+
+    :param images: generator object, series of images of a desk with plants
+    :param write_to_file: boolean, if True data would be written to file
+    :return: array of areas
+    '''
+    plants_growth = [[] for i in range(7)]
+    for desk in images:
+        parts = split_into_9_parts(desk['image'])
+        pixels_count = []
+        pixel_square = count_pixel_square(parts[4])
+
+        for n in range(9):
+            if (n == 4) or (n == 5):  # skipping reference square and empty part
+                continue
+
+            pixels_count.append(count_green_pixels(parts[n]))
+
+        squares = [c * pixel_square for c in pixels_count]
+
+        for plant in range(len(squares)):
+            plants_growth[plant].append({
+                'time': desk['time'],
+                'area': squares[plant]
+            })
+
+    if write_to_file:
+        for plant in range(len(plants_growth)):
+            write_areas_to_file(plants_growth[plant], plant)
+
+    return plants_growth
+
+
+def calculate_single_plant(images, plant_number, write_to_file=True):
+    '''
+    Calculates square of plants' leafs from one of the desks' parts[0...3, 6..8]
+
+    :param images: generator object, series of one desk images
+    :param plant_number:
+    :param write_to_file: boolean, if True data would be written to file
+    :return: series of plants' area
+    '''
+    area_series = []
+    for desk in images:
+        parts = split_into_9_parts(desk['image'])
+        pixel_square = count_pixel_square(parts[4])
+        # area_series.append(count_green_pixels(parts[plant_number]) * pixel_square)
+
+        area_series.append({
+            'time': desk['time'],
+            'area': (count_green_pixels(parts[plant_number]) * pixel_square)
+        })
+    if write_to_file:
+        write_areas_to_file(area_series, plant_number)
+
+    return area_series
--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import time
 import os
 from skimage import io
-from skimage.color import rgb2hsv
-from skimage.filters import threshold_otsu
-from skimage.filters.rank import median
-from skimage.morphology import disk
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
+import csv
+import pandas as pd
 
-
-def is_blurred(img):
-    variance = cv2.Laplacian(img, cv2.CV_64F).var()
-    return variance < 40
+import copy_selected_plant_photos
+import count_area
+import prediction
 
 
 def import_images(folder):
@@ -24,7 +20,7 @@ def import_images(folder):
     '''
     assert os.path.exists(folder)
     images_path = [f for f in os.listdir(folder) if os.path.splitext(f)[1] == '.jpg']
-    return (io.imread(os.path.join(folder, img)) for img in images_path)
+    return ({'time': img[:-4], 'image': io.imread(os.path.join(folder, img))} for img in images_path)
 
 
 def show_image(image):
@@ -47,120 +43,25 @@ def show_plots(data):
 
     plt.show()
 
-
-def split_into_9_parts(image):
-    '''
-    Splits image into 9 parts
-    |0|1|2|
-    |3|4|5|
-    |6|7|8|
-
-    :param image: numpy.ndarray
-    :return: list of 9 numpy.ndarrays
-    '''
-    vert = image.shape[0] // 3
-    hor = image.shape[1] // 3
-    ls = []
-
-    for col in range(3):
-        for row in range(3):
-            ls.append(image[vert * col: vert * (col + 1),
-                      hor * row: hor * (row + 1)])
-
-    return ls
+# Making dataset that consists of images of selected desks with plants
+# copy_selected_plant('all_photos', 6)
 
 
-def count_green_pixels(img):
-    hsv_image = rgb2hsv(img)
-    saturation = hsv_image[:, :, 1]
-    threshold = threshold_otsu(saturation)
-    saturation = saturation > threshold * 1.3
-    saturation = median(saturation, disk(2))  # filtering
-
-    # show_image(saturation)
-    pixels_count = saturation.mean() * saturation.shape[0] * saturation.shape[1] / saturation.max()
-    return pixels_count
+# Importing images from dataset
+# images = import_images('6_plant')
 
 
-def count_pixel_square(img):
-    '''
-    :param img: ndarray, - image with 2 reference squares
-    :return: one pixel square (cm^2)
-    '''
-    one_square = count_green_pixels(img) // 2
-    return 1 / one_square
+# Calculating areas of plants on the desk
+# count_area.calculate_squares(images)
 
 
-def calculate_squares(desk, storage=None):
-    '''
-    Adds calculated squares of plants' leafs from the desk to the storage
-    |0|1|2|
-    |3| | |
-    |4|5|6| i.e. excluding reference square and empty part
-
-    :param desk: image of a desk with plants
-    :param storage: array
-    :return: array of squares
-    '''
-    parts = split_into_9_parts(desk)
-    pixels_count = []
-    pixel_square = count_pixel_square(parts[4])
-
-    for n in range(9):
-        if (n == 4) or (n == 5):  # skipping reference square and empty part
-            continue
-
-        pixels_count.append(count_green_pixels(parts[n]))
-
-    squares = [c * pixel_square for c in pixels_count]
-
-    if desk is not None:
-        for part in range(7):
-            storage[part].append(squares[part])
-
-    return squares
+# Calculating areas of one selected plant on the desk
+# count_area.calculate_single_plant(images, 2)
 
 
-def calculate_single_plant(desk, plant_number):
-    '''
-    Calculates square of plants' leafs from one of the desks' parts[0...3, 6..8]
-
-    :param desk:
-    :param plant_number:
-    :return: square
-    '''
-    parts = split_into_9_parts(desk)
-    pixel_square = count_pixel_square(parts[4])
-    return count_green_pixels(parts[plant_number]) * pixel_square
+# Getting raw array with areas of one selected plant
+# show_plots([count_area.read_areas_from_file(2, type='array')])
 
 
-images = import_images('6_plant')
-
-plants_growth = [[] for i in range(7)]
-
-s_time = time.time()
-
-# counter = 25
-# for desk in images:
-#     if is_blurred(desk):
-#         continue
-#
-#     calculate_squares(desk, plants_growth)
-#
-#     counter -= 1
-#     if counter < 1:
-#         break
-#
-# for pl in plants_growth:
-#     pl = np.array(pl)
-#     print(pl.mean())
-#
-# show_plots(plants_growth)
-
-single_plant = []
-for desk in images:
-    single_plant.append(calculate_single_plant(desk, 2))
-
-show_plots([single_plant])
-
-print(str((time.time() - s_time) / 300) + ' per image')
+# Getting Pandas.Series with areas of one selected plant
+areas = count_area.read_areas_from_file(2)
